2c58b2a41e3bae1b8964deb42e61de636aa2e68b
xuesui <331063594@qq.com>
2020-04-24 12:17:13 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.sunnyweather.ui.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.example.sunnyweather.logic.Repository
+import com.example.sunnyweather.logic.model.Place
+
+class PlaceViewModel : ViewModel() {
+
+    private val searchLiveData = MutableLiveData<String>()
+
+    //对界面上显示的城市数据进行缓存
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = Transformations.switchMap(searchLiveData) { query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query: String) {
+        searchLiveData.value = query
+    }
+}
